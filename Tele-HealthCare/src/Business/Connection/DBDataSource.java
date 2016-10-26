90fe9ffc57a9daa628aaa0333a300166eb6d3f76
Guilherme Moresco Bisotto <guibisotto@gmail.com>
2016-10-26 03:35:32 -0200

    • Adicionando JAR do SQLite e Derby; • Criado DBDataSource, classe para gerar conexão com bando; • Criado bando no SQLite, e arquivo para criação do mesmo;

---
+++ Tele-HealthCare/src/Business/Connection/DBDataSource.java
@@ -0,0 +1,37 @@
+package Business.Connection;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+/**
+ *
+ * @author 11204974
+ */
+public class DBDataSource {
+    private static String DB_NAME = "DB_TECNICAS.db";
+    private static String DB_CONN_STRING = "jdbc:sqlite://" + System.getProperty("user.dir") + "/DB/" + DB_NAME;
+    private static String USER_NAME = "";
+    private static String PASSWORD = "";
+    private static DataSource dataSource;
+    private static Connection connection;
+    
+    public static Connection connectDB() throws Exception {
+        
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection(DB_CONN_STRING, USER_NAME, PASSWORD);
+        return connection;
+    }
+    
+    private static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBDataSource.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
